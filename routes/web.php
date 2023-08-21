a61931c7de62a41109c214baecfea9a0a6c95909
Besan-Ayman-AbuZubaida <hebaa2510@gmail.com>
2023-08-21 19:35:28 +0300

    add name

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('contact_us');
-});
+})->name('home');
 Route::post('send-email',[SendEmailController::class, 'sendEmail'])->name('send-email');
